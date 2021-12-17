c3503bc453dbef0b97f09b6bd61ba1a317349f66
johnny.woo <johnny.woo@hanyunsz.com>
2021-12-17 13:05:21 +0800

    regular merge from SmallChi

--- src/JT808.Protocol.Extensions/JT808.Protocol.Extensions.JT1078/MessageBody/JT808_0x1003.cs
+++ src/JT808.Protocol.Extensions/JT808.Protocol.Extensions.JT1078/MessageBody/JT808_0x1003.cs
@@ -1,6 +1,7 @@
 ﻿using JT808.Protocol.Formatters;
 using JT808.Protocol.Interfaces;
 using JT808.Protocol.MessagePack;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,43 +12,53 @@ namespace JT808.Protocol.Extensions.JT1078.MessageBody
     /// <summary>
     /// 终端上传音视频属性
     /// </summary>
+    [JsonObject(MemberSerialization.OptIn)]
     public class JT808_0x1003 : JT808Bodies, IJT808MessagePackFormatter<JT808_0x1003>, IJT808Analyze
     {
         /// <summary>
         /// 输入音频编码方式
         /// </summary>
+        [JsonProperty("audioEncode")]
         public byte EnterAudioEncoding { get; set; }
         /// <summary>
         /// 输入音频声道数
         /// </summary>
+        [JsonProperty("audioChannelCnt")]
         public byte EnterAudioChannelsNumber { get; set; }
         /// <summary>
         /// 输入音频采样率
         /// </summary>
+        [JsonProperty("audioSampleRate")]
         public byte EnterAudioSampleRate  { get; set; }
         /// <summary>
         /// 输入音频采样位数
         /// </summary>
+        [JsonProperty("audioSampleBit")]
         public byte EnterAudioSampleDigits { get; set; }
         /// <summary>
         /// 音频帧长度
         /// </summary>
+        [JsonProperty("audioFrameLen")]
         public ushort AudioFrameLength { get; set; }
         /// <summary>
         /// 是否支持音频输出
         /// </summary>
+        [JsonProperty("supportAudioOutput ")]
         public byte IsSupportedAudioOutput { get; set; }
         /// <summary>
         /// 视频编码方式
         /// </summary>
+        [JsonProperty("videoEncode")]
         public byte VideoEncoding { get; set; }
         /// <summary>
         /// 终端支持的最大音频物理通道数量
         /// </summary>
+        [JsonProperty("maxAudioChannels")]
         public byte TerminalSupportedMaxNumberOfAudioPhysicalChannels{ get; set; }
         /// <summary>
         /// 终端支持的最大视频物理通道数量
         /// </summary>
+        [JsonProperty("maxVideoChannels")]
         public byte TerminalSupportedMaxNumberOfVideoPhysicalChannels { get; set; }
         /// <summary>
         /// 
--- src/JT808.Protocol/MessageBody/JT808_0x8103_BodyBase.cs
+++ src/JT808.Protocol/MessageBody/JT808_0x8103_BodyBase.cs
@@ -1,7 +1,5 @@
 ﻿using JT808.Protocol.Interfaces;
 using JT808.Protocol.MessagePack;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -11,16 +9,19 @@ namespace JT808.Protocol.MessageBody
     /// <summary>
     /// 终端参数设置
     /// </summary>
-    public abstract class JT808_0x8103_BodyBase
+    public abstract class JT808_0x8103_BodyBase: IJT808Description
     {
         /// <summary>
         /// 参数 ID
         /// </summary>
         public abstract uint ParamId { get; set; }
-
         /// <summary>
         /// 参数长度
         /// </summary>
         public abstract byte ParamLength { get; set; }
+        /// <summary>
+        /// 消息描述
+        /// </summary>
+        public abstract string Description { get; }
     }
 }
