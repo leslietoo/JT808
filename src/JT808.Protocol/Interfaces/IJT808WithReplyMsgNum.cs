cff08775926af450d4ff20cc7007748386a6ba31
johnny.woo <johnny.woo@hanyunsz.com>
2022-05-19 21:59:20 +0800

    sniff service support

---
+++ src/JT808.Protocol/Interfaces/IJT808WithReplyMsgNum.cs
@@ -0,0 +1,17 @@
+﻿using JT808.Protocol.MessagePack;
+using System;
+using System.Text.Json;
+
+namespace JT808.Protocol.Interfaces
+{
+    /// <summary>
+    /// 带有平台消息流水号的808应答指令实现此接口
+    /// </summary>
+    public interface IJT808WithReplyMsgNum
+    {
+        /// <summary>
+        /// 平台下发的消息流水号
+        /// </summary>
+        ushort ReplyMsgNum { get; set; }
+    }
+}
