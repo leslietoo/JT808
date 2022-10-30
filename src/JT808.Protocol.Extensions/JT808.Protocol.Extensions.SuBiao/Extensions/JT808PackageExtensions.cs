592b0a5aeb4c4beef5e528bff7c0496c2e1bd011
johnny.woo <johnny.woo@hanyunsz.com>
2022-10-30 16:38:10 +0800

    Add support for alarm attachment

---
+++ src/JT808.Protocol.Extensions/JT808.Protocol.Extensions.SuBiao/Extensions/JT808PackageExtensions.cs
@@ -0,0 +1,94 @@
+﻿using JT808.Protocol.Enums;
+using JT808.Protocol.Extensions.SuBiao.Enums;
+
+namespace JT808.Protocol.Extensions
+{
+    /// <summary>
+    /// JT808创建包扩展
+    /// </summary>
+    public static partial class JT808PackageExtensions
+    {
+        /// <summary>
+        /// 根据消息Id创建包
+        /// </summary>
+        /// <typeparam name="TJT808Bodies"></typeparam>
+        /// <param name="msgId"></param>
+        /// <param name="terminalPhoneNo"></param>
+        /// <param name="bodies"></param>
+        /// <returns></returns>
+        public static JT808Package Create<TJT808Bodies>(this JT808_SuBiao_MsgId msgId, string terminalPhoneNo, TJT808Bodies bodies)
+            where TJT808Bodies : JT808Bodies
+        {
+            JT808Package jT808Package = new JT808Package
+            {
+                Header = new JT808Header
+                {
+                    MsgId = (ushort)msgId,
+                    TerminalPhoneNo = terminalPhoneNo,
+                },
+                Bodies = bodies
+            };
+            return jT808Package;
+        }
+        /// <summary>
+        /// 根据消息Id创建包
+        /// </summary>
+        /// <param name="msgId"></param>
+        /// <param name="terminalPhoneNo"></param>
+        /// <returns></returns>
+        public static JT808Package Create(this JT808_SuBiao_MsgId msgId, string terminalPhoneNo)
+        {
+            JT808Package jT808Package = new JT808Package
+            {
+                Header = new JT808Header
+                {
+                    MsgId = (ushort)msgId,
+                    TerminalPhoneNo = terminalPhoneNo,
+                }
+            };
+            return jT808Package;
+        }
+        /// <summary>
+        /// 根据消息Id创建2019版本包
+        /// </summary>
+        /// <typeparam name="TJT808Bodies"></typeparam>
+        /// <param name="msgId"></param>
+        /// <param name="terminalPhoneNo"></param>
+        /// <param name="bodies"></param>
+        /// <returns></returns>
+        public static JT808Package Create2019<TJT808Bodies>(this JT808_SuBiao_MsgId msgId, string terminalPhoneNo, TJT808Bodies bodies)
+            where TJT808Bodies : JT808Bodies
+        {
+            JT808Package jT808Package = new JT808Package
+            {
+                Header = new JT808Header
+                {
+                    MsgId = (ushort)msgId,
+                    TerminalPhoneNo = terminalPhoneNo,
+                },
+                Bodies = bodies
+            };
+            jT808Package.Header.MessageBodyProperty.VersionFlag = true;
+            return jT808Package;
+        }
+        /// <summary>
+        /// 根据消息Id创建2019版本包
+        /// </summary>
+        /// <param name="msgId"></param>
+        /// <param name="terminalPhoneNo"></param>
+        /// <returns></returns>
+        public static JT808Package Create2019(this JT808_SuBiao_MsgId msgId, string terminalPhoneNo)
+        {
+            JT808Package jT808Package = new JT808Package
+            {
+                Header = new JT808Header
+                {
+                    MsgId = (ushort)msgId,
+                    TerminalPhoneNo = terminalPhoneNo,
+                }
+            };
+            jT808Package.Header.MessageBodyProperty.VersionFlag = true;
+            return jT808Package;
+        }
+    }
+}
---
+++ src/JT808.Protocol.Extensions/JT808.Protocol.Extensions.SuBiao/MessageBody/JT808_0x12F0.cs
@@ -0,0 +1,21 @@
+﻿namespace JT808.Protocol.MessageBody
+{
+    /// <summary>
+    /// 苏标的报警附件的数据报文并没有包装在指令内，没有消息ID，这里伪造一个
+    /// </summary>
+    public class JT808_0x12F0 : JT808Bodies
+    {
+        /// <summary>
+        /// 跳过数据体序列化
+        /// </summary>
+        public override bool SkipSerialization { get; set; } = true;
+        /// <summary>
+        /// 0x12F0
+        /// </summary>
+        public override ushort MsgId { get; } = 0x12F0;
+        /// <summary>
+        /// 终端心跳
+        /// </summary>
+        public override string Description => "文件数据上传";
+    }
+}
